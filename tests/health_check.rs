79fc5a1a23239c915cf553e56ca417097bdf2285
Esakki <esakkimuthu6842@gmail.com>
2024-05-19 09:03:16 +0530

    Update health_check.rs

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -4,7 +4,6 @@ async fn spawn_app() -> std::io::Result<()>{
 
 #[tokio::test]
 async fn health_check_works(){
-    spawn_app().await.expect("Failed to spawn our app");
     let client = reqwest::Client::new();
     let response = client
                                      .get("http://127.0.0.1:8000/healthcheck")
