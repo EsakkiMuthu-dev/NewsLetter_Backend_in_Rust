998811e1af4c27d14138c45130c43952771b00e9
Esakki <esakkimuthu6842@gmail.com>
2024-05-26 23:13:23 +0530

    other tests for subscriptions endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,26 @@
 use std::net::TcpListener;
 
+use reqwest::Client;
+
 fn spawn_app() -> String{
      let listener = TcpListener::bind("127.0.0.1:0").expect("Unable to bound to random port ");
      let port = listener.local_addr().unwrap().port();
      let server = zero2prodLibrary::run(listener).expect("Unable to get server");
      let _ = tokio::spawn(server);
      format!("http://127.0.0.1:{}",port)
+}
+
+#[tokio::test]
+async  fn invalid_subscriptions_returns_400() {
+    let address = spawn_app();
+    
+    let body_contents = Vec![
+        ("name=bharathi","mail is missing"),
+        ("name=email=bharathi102000%40gmail.com","name is missing"),
+        ("","name is missing"),
+    ];
+    
+    
 }
 
 #[tokio::test]
@@ -21,3 +36,23 @@ async fn health_check_works(){
     assert!(response.status().is_success());
     assert_eq!(Some(0) , response.content_length())
 }
+
+#[tokio::test]
+async fn valid_subscriptions_returns_200()
+{
+    let app_address =spawn_app();
+    let client = Client::new();
+
+    let body ="name=bharathi&email=bharathi102000%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions",&app_address))
+        .header("Content-Type","application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    //assert
+    assert_eq!(200,response.status().as_u16())
+}
+
