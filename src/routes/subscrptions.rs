8fb166e97caeaaf3e06ccab219c75fca25ef20b5
Esakki <esakkimuthu6842@gmail.com>
2024-06-09 19:43:13 +0530

    Update subscrptions.rs

--- src/routes/subscrptions.rs
+++ src/routes/subscrptions.rs
@@ -11,17 +11,16 @@ pub struct FormData{
     email : String
 }
 
+#[tracing::instrument(
+name="Adding a new Subscriber",
+skip(form_data,connection_pool),
+fields(
+    request_id = %Uuid::new_v4(),
+    subscriber_email=%form_data.email,
+    subscriber_name=%form_data.name
+)
+)]
 pub async fn subscribe(form_data: web::Form<FormData>, connection_pool : web::Data<PgPool>) -> HttpResponse{
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!("Adding a new subscriber",
-        %request_id,
-        subscriber_email = %form_data.email,
-        subscriber_name = %form_data.name
-    );
-    let _request_span_guard = request_span.enter();
-    
-    tracing::info!("request_id -{} Adding '{}' '{}' as a new subscriber. " , request_id,form_data.email,form_data.name);
-    tracing::info!("request_id - {} Saving new Subscriber details in database ",request_id);
     
     let query_span = tracing::info_span!("Saving new Subscriber details into database");
     match sqlx::query!(
@@ -48,4 +47,32 @@ pub async fn subscribe(form_data: web::Form<FormData>, connection_pool : web::Da
         }
     }
     
+}
+
+#[tracing::instrument(
+"Saving a new Subscriber details to database",
+skip(form_data,connection_pool)
+)]
+
+pub async fn insert_subscriber(
+    connection_pool : &PgPool,
+    form_data: &FormData
+) -> Result<(),sqlx::Error>{
+    sqlx::query!(
+        r#"
+            INSERT Into subscriptions(id,email,name,subscribed_at)
+            VALUES ($1,$2,$3,$4)
+        "#,
+        Uuid::new_v4(),
+        form_data.email,
+        form_data.name,
+        Utc::now()
+    )
+        .execute(connection_pool)
+        .await
+        .map_err(|e|{
+            tracing::error!(" Failed to excecute query: {:?}",e);
+            e
+        })?;
+    Ok(())
 }
